74b4e1dbe72b5cf0e0c861723be1986dcbdb83a5
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-15 14:54:54 +0000

    route all_products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ Return a page which shows all products """
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/index.html', context)
