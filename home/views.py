1d7b4844b5a107cac2404ca2266dcafaca09f2ec
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-15 14:40:17 +0000

    style home page displaying categories

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from products.models import Category
 
 # Create your views here.
 
 def index(request):
-    """ Return the home page """
-    return render(request, 'home/index.html')
+    """ Return the home page which shows product categories """
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'home/index.html', context)
