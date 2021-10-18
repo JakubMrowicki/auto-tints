d749d6772dd7e4de59a890196a560afe3aa558c7
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-18 14:19:14 +0000

    add adding to cart functionality

--- auto_tints/context_processors.py
+++ auto_tints/context_processors.py
@@ -11,12 +11,18 @@ def get_categories(request):
 
 
 def cart_contents(request):
-    cart_items = {}
+    cart_items = request.session.get('cart', {})
+    cart_items_count = 0
     total = 0
     grand_total = total + settings.DELIVERY_CHARGE
 
+    if cart_items:
+        for quantity in list(cart_items.values()):
+            cart_items_count += int(quantity)
+
     context = {
         'cart_items': cart_items,
+        'cart_items_count': cart_items_count,
         'total': total,
         'grand_total' : grand_total,
         'delivery_charge': settings.DELIVERY_CHARGE,
