74ebd5d6108a05a08022343ca903f7ea9e5f9ece
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-24 13:12:09 +0000

    secure admin only areas

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
@@ -11,6 +12,7 @@ def faq(request):
     return render(request, 'pages/faq.html')
 
 
+@login_required
 def profile(request):
     """ Return Profile page """
 
