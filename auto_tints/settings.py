1926b9cf0b659010aedb7ce59a3e9d4c04750a1b
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-25 14:32:01 +0000

    add gunicorn and set Procfile

--- auto_tints/settings.py
+++ auto_tints/settings.py
@@ -129,12 +129,17 @@ WSGI_APPLICATION = 'auto_tints.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse('DATABASE_URL')
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
