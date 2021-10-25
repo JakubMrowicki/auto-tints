3f272a1107d82c474c7218d9a6ff58d91563109f
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-25 14:45:38 +0000

    set debug to false in prod

--- auto_tints/settings.py
+++ auto_tints/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT', 'True')
-# DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['auto-tints.herokuapp.com', 'localhost']
 # Change For Prod ################################################################################
