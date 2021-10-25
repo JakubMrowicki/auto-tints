bb885762d02da1c22ed4fa70279e117bc95ff412
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-10-25 14:40:57 +0000

    update database url

--- auto_tints/settings.py
+++ auto_tints/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = 'auto_tints.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse('DATABASE_URL')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
     }
 else:
     DATABASES = {
